c514c25d2b9a91f17ab6a1ee5d2fefc9d42b77de
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-01-18 21:22:02 -0600

    Completed the CollisionDetection folder

--- Engine/CollisionDetection/Circle.cs
+++ Engine/CollisionDetection/Circle.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
 
-namespace Engine.CollisionDetection
+namespace Engine
 {
-    internal class Circle
+    public struct Circle
     {
+        public float Radius
+        {
+            get;
+            private set;
+        }
+        public Vector2 Center
+        {
+            get;
+            private set;
+        }
+        Circle(float radius, Vector2 center)
+        {
+            Radius = radius;
+            Center = center;
+        }
     }
 }
--- Engine/CollisionDetection/CollisionDetection.cs
+++ Engine/CollisionDetection/CollisionDetection.cs
@@ -1,6 +1,56 @@
-﻿namespace Engine.CollisionDetection
+﻿using Microsoft.Xna.Framework;
+
+namespace Engine
 {
     public class CollisionDetection
     {
+        #region Public Methods
+        /// <summary>
+        /// Checks and returns whether two circles interseet
+        /// </summary>
+        /// <param name="circle1">A circle of some object</param>
+        /// <param name="circle2">A circle of some other object</param>
+        /// <returns></returns>
+        public static bool ShapesIntersect(Circle circle1, Circle circle2)
+        {
+            return Vector2.Distance(circle1.Center, circle2.Center) <= circle1.Radius + circle2.Radius;
+        }
+        /// <summary>
+        /// Check and return whether two rectangles intersect
+        /// </summary>
+        /// <param name="rectangle1">A rectangle of some object</param>
+        /// <param name="rectangle2">A rectangle of some other object</param>
+        /// <returns></returns>
+        public static bool ShapesIntersect(Rectangle rectangle1, Rectangle rectangle2)
+        {
+            return rectangle1.Intersects(rectangle2);
+        }
+        /// <summary>
+        /// Check and returns whether a a rectangle and a circle intersect
+        /// </summary>
+        /// <param name="rectangle"></param>
+        /// <param name="circle"></param>
+        /// <returns></returns>
+        public static bool ShapesIntersect(Rectangle rectangle, Circle circle)
+        {
+            Vector2 closestPoint;
+            closestPoint.X = MathHelper.Clamp(circle.Center.X, rectangle.Left, rectangle.Right);
+            closestPoint.Y = MathHelper.Clamp(circle.Center.Y, rectangle.Top, rectangle.Bottom);
+            return Vector2.Distance(circle.Center, closestPoint) <= circle.Radius;
+        }
+        public static Rectangle CalculateIntersection(Rectangle rect1, Rectangle rect2)
+        {
+            if (!ShapesIntersect(rect1, rect2))
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+            int xmin = Math.Max(rect1.Left, rect2.Left);
+            int xmax = Math.Min(rect1.Right, rect2.Right);
+            int ymin = Math.Max(rect1.Top, rect2.Top);
+            int ymax = Math.Min(rect1.Bottom, rect2.Bottom);
+            return new Rectangle(xmin, ymin, xmax - xmin, ymax - ymin);
+        }
+
+        #endregion
     }
 }
